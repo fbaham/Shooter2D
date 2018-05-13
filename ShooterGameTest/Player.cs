c369215d42a2d3a46769c6fb706dcc2d8ad11258
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-05-12 21:08:21 -0300

    first commit

---
+++ ShooterGameTest/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Shooter
+{
+    class Player
+    {
+        //Animación para representar al jugador
+        public Texture2D PlayerTexture;
+
+        public void Initialize()
+
+        {
+
+        }
+
+
+
+        public void Update()
+
+        {
+
+        }
+
+
+
+        public void Draw()
+
+        {
+
+        }
+    }
+}
