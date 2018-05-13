48b033853b885390c75a5a326b8d92c0401741af
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-05-12 22:47:40 -0300

    controles

--- ShooterGameTest/Game1.cs
+++ ShooterGameTest/Game1.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
 using Shooter;
+using Microsoft.Xna.Framework.Input.Touch;
 
 namespace ShooterGameTest
 {
@@ -15,8 +15,22 @@ namespace ShooterGameTest
         SpriteBatch spriteBatch;
 
         // Represents the player
-
         Player player;
+
+        // Keyboard states used to determine key presses
+        KeyboardState currentKeyboardState;
+        KeyboardState previousKeyboardState;
+
+        // Gamepad states used to determine button presses
+        GamePadState currentGamePadState;
+        GamePadState previousGamePadState;
+
+        //Mouse states used to track Mouse button press
+        MouseState currentMouseState;
+        MouseState previousMouseState;
+
+        // A movement speed for the player
+        float playerMoveSpeed;
 
         public Game1()
         {
@@ -38,6 +52,12 @@ namespace ShooterGameTest
 
             player = new Player();
 
+            // Set a constant player move speed
+            playerMoveSpeed = 8.0f;
+
+            //Enable the FreeDrag gesture.
+            TouchPanel.EnabledGestures = GestureType.FreeDrag;
+
             base.Initialize();
         }
 
@@ -53,9 +73,7 @@ namespace ShooterGameTest
             // TODO: use this.Content to load your game content here
 
             // Load the player resources
-
             Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
-
             player.Initialize(Content.Load<Texture2D>("Graphics\\player"), playerPosition);
 
         }
@@ -79,9 +97,74 @@ namespace ShooterGameTest
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Save the previous state of the keyboard and game pad so we can determine single key/button presses
+            previousGamePadState = currentGamePadState;
+            previousKeyboardState = currentKeyboardState;
+
+            // Read the current state of the keyboard and gamepad and store it
+            currentKeyboardState = Keyboard.GetState();
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+            //Update the player
+            UpdatePlayer(gameTime);
 
             base.Update(gameTime);
+        }
+
+        private void UpdatePlayer(GameTime gameTime)
+
+        {
+            // Windows 8 Touch Gestures for MonoGame
+            while (TouchPanel.IsGestureAvailable)
+            {
+                GestureSample gesture = TouchPanel.ReadGesture();
+
+                if (gesture.GestureType == GestureType.FreeDrag)
+                {
+                    player.Position += gesture.Delta;
+                }
+            }
+
+            //Get Mouse State then Capture the Button type and Respond Button Press
+            Vector2 mousePosition = new Vector2(currentMouseState.X, currentMouseState.Y);
+
+            if (currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                Vector2 posDelta = mousePosition - player.Position;
+                posDelta.Normalize();
+                posDelta = posDelta * playerMoveSpeed;
+                player.Position = player.Position + posDelta;
+            }
+
+            // Get Thumbstick Controls
+            player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+            player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+            // Use the Keyboard / Dpad
+            if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+            {
+                player.Position.X -= playerMoveSpeed;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+            {
+                player.Position.X += playerMoveSpeed;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+            {
+                player.Position.Y -= playerMoveSpeed;
+            }
+
+            if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+            {
+                player.Position.Y += playerMoveSpeed;
+            }
+
+            // Make sure that the player does not go out of bounds
+            player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+            player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+
         }
 
         /// <summary>
@@ -92,22 +175,13 @@ namespace ShooterGameTest
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-
             // Start drawing
-
             spriteBatch.Begin();
 
-
-
             // Draw the Player
-
             player.Draw(spriteBatch);
 
-
-
             // Stop drawing
-
             spriteBatch.End();
 
             base.Draw(gameTime);
